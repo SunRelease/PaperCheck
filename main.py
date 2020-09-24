163e2948bb457fa2e84add0bd3c62ee292e77e27
SunRelease <cacker-opener@139.com>
2020-09-24 17:25:38 +0800

    1.新增SimHash类函数get_Hash(用来对文章进行分词,降维操作).

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 # author :HXM
-
 from sys import argv
 import math, logging
 import jieba
@@ -36,6 +35,43 @@ class SimHash(object):
             x = bin(x).replace('0b', '').zfill(64)[-64:]
             return str(x)
 
+    def getHash(self, rawstr):
+        '''
+        对文章进行分词，降维
+        :param rawstr:
+        :return:
+        '''
+        seg = jieba.cut(rawstr)
+        keywords = jieba.analyse.extract_tags("|".join(seg), topK=100, withWeight=True)
+        ret = []
+        for keyword, weight in keywords:
+            '''
+            获取关键字和权重
+            '''
+            source_str = self.get_source_string(keyword)
+            keylists = []
+
+            for c in source_str:
+                weight = math.ceil(weight)
+                if c == "1":
+                    keylists.append(int(weight))
+                else:
+                    keylists.append(-int(weight))
+            ret.append(keylists)
+        rows = len(ret)
+        # 对列表进行"降维"
+        cols = len(ret[0])
+        result = []
+        for i in range(cols):
+            tmp = 0
+            for j in range(rows):
+                tmp += int(ret[j][i])
+            if tmp > 0:
+                tmp = "1"
+            elif tmp <= 0:
+                tmp = "0"
+            result.append(tmp)
+        return "".join(result)
 
 
 
